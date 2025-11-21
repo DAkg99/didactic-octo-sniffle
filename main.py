43b241a593f5b11617a8f0ab748262ffa5b2cafb
unknown <test@example.com>
2025-11-21 16:30:17 +0300

    Added rough schedule viewing

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import movies
-import seating
-import bookings
-import storage
-import reports
+# import seating
+# import bookings
+# import storage
+# import reports
 
+# Database path
+data_path = "./data/"
 
 # Actions for menus
 main_actions = { 
@@ -12,6 +14,12 @@ main_actions = {
     "2": {"value": "bookings", "description": "Book viewing"},
     "3": {"value": "unbook", "description": "Cancel booking"}
     }
+schedule_actions = { 
+    "0": {"value": "back", "description": "[Go back]"},
+    "1": {"value": "name", "description": "Showtimes of a specific movie"},
+    "2": {"value": "date", "description": "All movies on a specific day"},
+    "3": {"value": "all", "description": "All movies"}
+    }
 
 
 # Functions for menu navigation and providing user with information
@@ -19,32 +27,50 @@ def print_actions(possible_actions: dict) -> None:
     """Prints possible actions user can take."""
     for key, value in possible_actions.items():
         print(key,value["description"])
-    
+        
 def make_user_pick_action(possible_actions: dict) -> str:
     """Prompts user to pick an action, returns 'invalid' if not valid."""
-    return possible_actions.get(input("Enter a number: "),"invalid")
+    result = possible_actions.get(input("Enter a number: "),"invalid")
+    if result == "invalid":
+        return result
+    return result["value"]
 
-def do_menu(user_prompt,menu_actions):
+def do_menu(user_prompt: str,menu_actions: dict) -> str:
+    """Shows a menu with given prompt & actions, asks user to pick action."""
     print(user_prompt)
     print_actions(menu_actions)
-    user_action = make_user_pick_action(menu_actions)
-    return user_action
+    user_choice = make_user_pick_action(menu_actions)
+    return user_choice
     
     
+# Run stuff
 while True:
-    user_choice = do_menu("What would you like to do?", main_actions)
-    match user_choice:
-        case "invalid":
-            print("Invalid selection.\n")
+    user_action = do_menu("What would you like to do?", main_actions)
+    
+    if user_action == "invalid":
+        print("Invalid selection.\n")
+        continue
+        
+    elif user_action == "schedule":
+        user_action = do_menu("\nWhich showtimes would you like to see?", schedule_actions)
+        if user_action == "back":
             continue
-        case "schedule":
-            pass
-        case "booking":
-            pass
-        case "unbooking":
-            pass
-        case "admin":
-            pass
-        case _:
-            pass
+        if user_action == "all":
+            user_want = None
+        else:
+            user_want = input(f"Enter {user_action}: ")
+        result = movies.list_showtimes(data_path,user_action,user_want)
+        for item in result:
+            print(item)
+        input("\nEnter to continue")
+        continue
+        
+    elif user_action == "booking":
+        pass
+    elif user_action == "unbooking":
+        pass
+    elif user_action == "admin":
+        pass
+    else:
+        pass
             
--- movies.py
+++ movies.py
@@ -1,14 +1,38 @@
 import json
 
-def load_movies(path: str) -> list: 
-    return json.load(open(path))
+def load_movies(path: str) -> list:
+    """Loads movie database file"""
+    return list(json.load(open(path+"movies.json")))
 
 def save_movies(path: str, movies: list) -> None:
-    json.dump(movies, open(path,"w"))
+    """Saves movies to database file"""
+    json.dump(movies, open(path+"movies.json","w"))
 
-def add_movie(movies: list, movie_data: dict) -> dict: # Why is output dict??
-    ...
+def add_movie(movies: list, movie_data: dict) -> list:
+    """Adds a new movie to the list of movies"""
+    movies.append(movie_data)
+    return movies
+    
+def load_showtimes(path: str) -> list:
+    """Loads showtime database file"""
+    return list(json.load(open(path+"showtimes.json")))
 
-def schedule_showtime(showtimes: list, showtime_data: dict) -> dict: ...
-def list_showtimes(showtimes: list, movie_id: str | None = None, date: str| None = None) -> list: ...
+
+def list_showtimes(path: str, request_type: str, request: str):
+    showtimes = load_showtimes(path)
+    if request_type == "all":
+        return showtimes
+    requested_showtimes = []
+    for item in showtimes:
+        if item[request_type] == request:
+            requested_showtimes.append(item)
+    if not requested_showtimes:
+        return "No showings found."
+    else:
+        return requested_showtimes
+    
+def schedule_showtime(showtimes: list, showtime_data: dict) -> dict:
+    pass
+    
+# def list_showtimes(showtimes: list, movie_id: str | None = None, date: str| None = None) -> list: ...
 def update_showtime(showtimes: list, showtime_id: str, updates: dict) -> dict: ...
